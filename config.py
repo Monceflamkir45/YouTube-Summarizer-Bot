0e56a43f1147539875874fdf1f996727019d9db7
Monceflamkir45 <spicybrawl5@gmail.com>
2024-09-04 16:06:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Telegram:
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    AUTH_USER_ID = int(os.environ.get('AUTH_USER_ID'))
+    API_ID = int(os.environ.get('22784345'))
+    API_HASH = os.environ.get('5fe063ba8f41c81043b9abb9a6bbab17')
+    BOT_TOKEN = os.environ.get('7515355666:AAHnKhhpkk5f8NK6aLqhK3DoO2bpQfqPPJg')
+    AUTH_USER_ID = int(os.environ.get('6097677058'))
     
 class Ai:
-    GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
+    GROQ_API_KEY = os.environ.get('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')
 
 class Database:
     REDIS_URI = os.environ.get('REDIS_URI')
